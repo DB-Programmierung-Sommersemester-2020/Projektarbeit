9127a6c4a5ec4dec06fdc033a1ed35a94943c500
VChouliak <chouliak@hotmail.com>
2020-05-30 08:48:48 +0200

    Implement user - login

--- Bookshop/src/servlet/LoginServlet.java
+++ Bookshop/src/servlet/LoginServlet.java
@@ -7,23 +7,51 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import bookshop.controllers.CustomerController;
+import bookshop.helpers.UserInputCheckHelper;
+import model.shop.CustomerView;
+import model.shop.ModelFacade;
+
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	private CustomerController customerController = null;
+	private ModelFacade viewModelFacade = null;
 
-    public LoginServlet() {
-        super();
-        
-    }
+	public LoginServlet() {
+		super();
 
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
 	}
 
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		this.customerController = CustomerController.getInstance();
+		this.viewModelFacade = ModelFacade.getInstance();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String email = request.getParameter("user");
+		String pwd = request.getParameter("passwd");
+		String message ="";
+		if (!UserInputCheckHelper.attributesOrAttributeEmpty(email,pwd)) {
+			CustomerView customer = viewModelFacade.getCustomerById(email);
+
+			if (customerController.checkPassword(customer, pwd)) {
+				getServletContext().getRequestDispatcher("/kaufinformation.jsp").forward(request, response);
+			}else {
+				message="Login war nicht erfolgreich, Benutzername oder Kennwort ist falsch...";
+			}
+		}
+		message="Eingabe war nicht vollst&aumlndig, bitte alle Felder ausf&uuml;llen...";
+		request.setAttribute("message", message );
+		getServletContext().getRequestDispatcher("/errorPage.jsp").forward(request, response);
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doGet(request, response);
 	}
 
